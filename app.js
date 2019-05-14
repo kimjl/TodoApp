cd514a7fe90af9c7a636e195ea7a9a1277387a99
kimjl <jamesleekim90@gmail.com>
2019-05-14 18:43:53 -0400

    Added backend for data validation

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const db = require('./db');
 const collection = 'todo'
 const app = express();
 
+// schema used for data validation for our todo document
+const schema = Joi.object().keys({
+    todo: Joi.string().required()
+});
+
 app.use(bodyParser.json());
 
 // serve static html file to user
@@ -42,14 +47,27 @@ app.put('/:id', (req, res) => {
 });
 
 // create
-app.post('/', (req, res) => {
+app.post('/', (req, res, next) => {
   const userInput = req.body;
-  db.getDB().collection(collection).insertOne(userInput, (err, result) => {
-    if (err)
-        console.log(err);
-    else
-        res.json({result: result, document: result.ops[0]});
-  });
+  Joi.validate(userInput, schema, (err, result) => {
+      if(err) {
+          const error = new Error('Invalid Input');
+          error.status = 400;
+          next(error);
+      }
+      else {
+          db.getDB().collection(collection).insertOne(userInput, (err, result) => {
+              if(err) {
+                  const error = new Error('Failed to insert Todo document');
+                  error.status = 400;
+                  next(error);
+              }
+              else
+                  res.json({result: result, document: result.ops[0], msg: 'Successfully inserted Todo', error: null});
+            });
+      }
+  })
+
 });
 
 // delete
@@ -62,6 +80,16 @@ app.delete('/:id', (req, res) => {
         res.json(result);
   });
 });
+
+// Middleware for handling Error
+// Sends Error Response Back to User
+app.use((err,req,res,next)=>{
+    res.status(err.status).json({
+        error : {
+            message : err.message
+        }
+    });
+})
 
 db.connect((err) => {
     // If err unable to connect to database
